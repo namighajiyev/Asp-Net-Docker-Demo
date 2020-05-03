a0e9991020a321afd69ca3c4586c7e57f2e7fc49
Namig Hajiyev <namiq886@gmail.com>
2020-05-03 23:32:16 +0400

    run app with bind mount

--- Tools/run-in-docker.sh
+++ Tools/run-in-docker.sh
@@ -1,2 +1,2 @@
-docker build -t asp-net-docker-app .
-docker run -it --rm -p 8080:80 asp-net-docker-app
+docker build -t asp-net-docker-demo-app .
+docker run -it --rm -p 8080:80 asp-net-docker-demo-app
